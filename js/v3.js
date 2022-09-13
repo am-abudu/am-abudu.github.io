8be7b067054ba76cca9da9ae00df9e009a9998d7
Abudu <abudulin@foxmail.com>
2022-09-13 23:12:03 +0800

    1

---
+++ js/v3.js
@@ -0,0 +1,8 @@
+/**
+ * Minified by jsDelivr using Terser v5.14.1.
+ * Original file: /gh/am-abudu/kaygb@master/js/v3.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+ var windowWidth=$(window).width();windowWidth<767&&$("#aplayer").remove();var meting_api="https://api.mizore.cn/meting/api.php?server=:server&type=:type&id=:id";console.log("\n %c PersonalWebSite by 风也温柔  %c https://eas1.cn/posts/165.html \n\n","color: #fff; background: #fd79a8; padding:5px 0;","background: #FFF; padding:5px 0;");
+ //# sourceMappingURL=/sm/59516ea22fb7651b5c0fc852e77251371b134c467e5ed8c85ac2867a3a2fbb85.map
